759e2eefb33562cef9a183a43a6441a9dff7653d
Julyan Widianto <july@neuronworks.co.id>
2017-05-25 14:13:41 +0700

    second commit - add method

--- HitungLuas.java
+++ HitungLuas.java
@@ -1,5 +1,9 @@
 public class HitungLuas {
     public static void main(String[] args) {
+        System.out.println(cariLuasPersegiPanjang(10, 5));
+    }
 
+    public static int cariLuasPersegiPanjang(int panjang, int lebar){
+        return panjang*lebar;
     }
 }
